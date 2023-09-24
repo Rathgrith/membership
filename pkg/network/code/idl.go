8d6376bf614d6bcf47a30d9bb6033f277e337eac
feize <feizes2@illinois.edu>
2023-09-24 17:02:59 -0500

    add runmode

--- pkg/network/code/idl.go
+++ pkg/network/code/idl.go
@@ -2,6 +2,13 @@ package code
 
 import "time"
 
+type RunMode int
+
+const (
+	PureGossip RunMode = iota + 1
+	GossipWithSuspicion
+)
+
 type MethodType int32
 
 const (
@@ -17,6 +24,7 @@ type MemberStatus int
 const (
 	Alive MemberStatus = iota + 1
 	Failed
+	Suspected
 )
 
 type JoinRequest struct {
@@ -46,4 +54,18 @@ type HeartbeatRequest struct {
 	MemberShipList map[string]*MemberInfo `json:"sub_member_ship_list"`
 	SuspicionFlag  bool                   `json:"suspicion_flag"`
 	UpdateTime     time.Time              `json:"update_local_time"`
+}
+
+type SuspensionInfoType int
+
+const (
+	Suspect SuspensionInfoType = iota + 1
+	InformAlive
+	ConfirmFailed
+)
+
+type SuspensionRequest struct {
+	TargetID          string             `json:"target_id"`
+	InfoType          SuspensionInfoType `json:"info_type"`
+	IncarnationNumber int                `json:"incarnation_number"`
 }
