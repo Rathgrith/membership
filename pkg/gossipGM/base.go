4dab5ab5540f7870f5ac6b2afe06170b20195687
dl58 <dl58@illinois.edu>
2023-09-24 19:07:24 -0500

    enable/disable

--- pkg/gossipGM/base.go
+++ pkg/gossipGM/base.go
@@ -198,6 +198,7 @@ func (s *Service) detectionRoutine() {
 		UpdateTime:     s.timeStamp,
 		SuspicionFlag:  flag,
 	}
+	logutil.Logger.Infof("heartbeat request:%v", r)
 	for _, neighborHost := range selectedNeighbors {
 		req := network.CallRequest{
 			MethodName: code.Heartbeat,
