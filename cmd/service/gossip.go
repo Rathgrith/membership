f0fed42fc94fb2605ecb1564af742c65e015c0ad
feize <feizes2@illinois.edu>
2023-10-26 18:33:42 -0500

    add comment

--- cmd/service/gossip.go
+++ cmd/service/gossip.go
@@ -8,24 +8,31 @@ import (
 )
 
 func main() {
+	// log is just for test, should not be used in prod env
 	err := logutil.InitDefaultLogger(logrus.DebugLevel)
 	if err != nil {
 		panic(err)
 	}
 
 	service, err := ece428_mp2.NewDefaultGossipGMService()
-	service.JoinToGroup([]string{"fa23-cs425-4801.cs.illinois.edu"})
+	service.JoinToGroup([]string{"fa23-cs425-4801.cs.illinois.edu"}) // join a group explicitly
 	if err != nil {
 		panic(err)
 	}
 
+	// subscribe member failed event
 	failListenChan := make(chan string, 10)
 	service.SubscribeFailNotification(nil, true, failListenChan)
-	go func() {
+	go func() { // handle failed event
 		for {
 			failedHost := <-failListenChan
 			fmt.Println(fmt.Sprintf("%v failed", failedHost))
 		}
 	}()
+
+	// get all alive hosts
+	fmt.Println(service.GetHostsOfAllMembers())
+
+	// start
 	service.Serve()
 }
