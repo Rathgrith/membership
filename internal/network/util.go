1c8168be72ac043344ab49904062e3124aea8b29
feize <feizes2@illinois.edu>
2023-10-26 16:55:25 -0500

    fix

--- internal/network/util.go
+++ internal/network/util.go
@@ -1,25 +1,8 @@
 package network
 
 import (
-	"ece428_mp2/config"
-	"fmt"
-	"net"
 	"os"
 )
-
-func CleanUDPReceiveBuffer() {
-	listen, err := net.ListenUDP("udp", &net.UDPAddr{
-		IP:   net.IPv4(0, 0, 0, 0),
-		Port: config.GetListenPort(),
-	})
-	if err != nil {
-		panic(err)
-	}
-	defer listen.Close()
-
-	var data [1024]byte
-	fmt.Println(listen.ReadFromUDP(data[:]))
-}
 
 func GetSelfHostName() string {
 	hostname, err := os.Hostname()
